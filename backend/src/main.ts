8162e512aed03c412f2a6cdf8df87d4e66e79a15
Shae West <shaewest02@gmail.com>
2024-06-16 12:40:41 +1200

    env port support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
